57c7094983cabd324a9c2533d723e93a1b19a9d2
Prakhar Sharma <prakhar.infinity@gmail.com>
2023-07-27 23:01:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './Components/Signup';
+
+function App() {
+  return (
+    <>
+    <Signup />
+    </>
+  );
+}
+
+export default App;
